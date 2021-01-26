24a89098fccb7d90f12a3a3fd64bb6f7490257f9
burnpiro <kemalpiro@gmail.com>
2021-01-26 16:59:40 +0100

    Fix build

--- web/src/hooks/useAPI.js
+++ web/src/hooks/useAPI.js
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import useWebSocket, {ReadyState} from "react-use-websocket";
 import useInterval from "./useInterval";
 
